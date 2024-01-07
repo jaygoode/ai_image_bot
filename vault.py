7189f1897cb4aba812a7f45ae524d4db97f91ad3
Johnny Nylund <91679773+jaygoode@users.noreply.github.com>
2024-01-07 21:41:33 +0200

    added a Vault class to vault file, with multiple methods to auth to client and get secrets

--- vault.py
+++ vault.py
@@ -1,13 +1,32 @@
 import hvac
-import logging
-
-logger = logging.getLogger("my_logger")
-logger.setLevel(logging.DEBUG)
+from logger_setup import logger
 
 
-token = ""
-url = ""
-client = hvac.Client(url=url, token=token)
+class Vault:
+    def __init__(self, token: str, url: str):
+        self.client = self.authenticate_client(token, url)
 
-if not client.is_authenticated():
-    print
+    def authenticate_client(self, token: str, url: str):
+        client = hvac.Client(url=url, token=token)
+
+        if not client.is_authenticated():
+            logger.error("Vault client could not authenticate.")
+
+        return client
+
+    def create_or_update_secret(self, username: str, password: str):
+        self.client.secrets.kv.v2.create_or_update_secret(
+            path="secret/website_credentials",
+            secret=dict(username=username, password=password),
+        )
+
+    # path = "secret/website_credentials"
+    def get_credentials(self, path: str):
+        data = self.client.secrets.kv.v2.read_secret_version(path=path)
+
+        if data and "data" in data:
+            username = data["data"]["data"]["username"]
+            password = data["data"]["data"]["password"]
+            return (username, password)
+        else:
+            logger.error("Credentials not found in Vault")
