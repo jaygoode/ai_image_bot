c61206c2ef6cd0c0417fddb115e1946f7f8f0407
Johnny Nylund <91679773+jaygoode@users.noreply.github.com>
2024-01-08 21:12:27 +0200

    added multiple functions for selenium, login create post

--- selenium_functions.py
+++ selenium_functions.py
@@ -6,16 +6,33 @@ from selenium.webdriver.chrome.service import Service
 
 class Selenium():
     def __init__():
-        pass
-    
-    def open_instagram():
         chrome_driver_path = 'C:\Users\johnny\Downloads\chromedriver_win32'
         chrome_service = Service(chrome_driver_path)
         driver = webdriver.Chrome(service=chrome_service)
-
+    
+    def open_instagram(self):
         url = 'https://www.instagram.com/'
+        self.driver.get(url)
+        home_nav_xpath = "//span[contains(text(), 'Home')]"
+        home_button = self.driver.find_elements(By.XPATH, home_nav_xpath)
+        home_button.click()
+        if not home_button:
+            self._login()
 
-        driver.get(url)
+    def create_post(self, image_path):
+        create_nav_xpath = "//span[contains(text(), 'Create')]"
+        post_nav_xpath = "//span[contains(text(), 'Post')]"
+        create_button = self.driver.find_elements(By.XPATH, create_nav_xpath)
+        post_button = self.driver.find_elements(By.XPATH, post_nav_xpath)
+        create_button.click()
+        post_button.click()
+        select_xpath = "//span[contains(text(), 'Select from computer')]"
+        select_button = self.driver.find_elements(By.XPATH, select_xpath)
+        select_button.click()
+        file_input = self.driver.find_element(By.XPATH, '//input[@type="file"]')
+        file_input.send_keys(image_path)
 
-    def login(username, password):
-        pass
+    def _login(self, username=None, password=None):
+        continue_as_xpath = "//span[contains(text(), 'Continue as ')]/parent::button"
+        login_button = self.driver.find_element(By.XPATH, continue_as_xpath)
+        login_button.click()
