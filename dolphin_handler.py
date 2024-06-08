06b217973355af70c9a9bc820ed5c45b6afaac9a
Johnny Nylund <91679773+jaygoode@users.noreply.github.com>
2024-06-09 02:07:44 +0300

    fixed some return statements and added type hints to dolphin handler

--- dolphin_handler.py
+++ dolphin_handler.py
@@ -8,20 +8,21 @@ class AiHandler:
         self.image_folder = image_folder
         # self.ollama_client = ollama.OllamaClient()
 
-    def chat(self, message):
+    def chat(self, message:str) => str:
         messages = [
             {
                 "role": "user",
                 "content": message,
             },
         ]
-
+        response = ""
         for part in chat(model="dolphin-mixtral", messages=messages, stream=True):
             print(part["message"]["content"], end="", flush=True)
-
+            response += part["message"]["content"]
+        return response
         print()
 
-    def ask_about_image(self, question, image_file):
+    def ask_about_image(self, question:str, image_file:str) => str:
         # TODO this does not yet work
         image_fp = os.path.join(self.image_folder, image_file)
         message = {
@@ -37,11 +38,10 @@ class AiHandler:
         )
 
         # Print the model's description of the image
-        breakpoint()
         print(response["message"]["content"])
+        return response["message"]["content"]
 
 
-image_folder = f"C:\\Users\\johnny\\Desktop\\repos\\personal_repos\\fooocus\\Fooocus-api\\Fooocus-API\\outputs\\files\\2024-05-15"
 image_folder = f"C:\\Users\\johnny\\Desktop\\repos\\personal_repos\\fooocus\\Fooocus-api\\Fooocus-API\\outputs\\files\\2024-05-15"
 ai = AiHandler(image_folder)
 image = "test.png"
